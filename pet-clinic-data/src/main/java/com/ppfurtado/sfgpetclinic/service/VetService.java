8288b2e2e76c4137a2664661b90dc84feb42b7a4
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-07-26 13:49:47 -0300

    Implement Interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/service/VetService.java
@@ -0,0 +1,14 @@
+package com.ppfurtado.sfgpetclinic.service;
+
+import com.ppfurtado.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
