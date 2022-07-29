aeefecd27d54f48bbea31f3c8822439ca302aae4
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-07-29 07:47:17 -0300

    adding layer map service

---
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/service/map/OwnerServiceMap.java
@@ -0,0 +1,34 @@
+package com.ppfurtado.sfgpetclinic.service.map;
+
+import com.ppfurtado.sfgpetclinic.model.Owner;
+import com.ppfurtado.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+}
