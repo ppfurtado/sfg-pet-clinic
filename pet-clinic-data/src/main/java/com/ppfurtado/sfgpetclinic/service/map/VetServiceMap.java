53cf8c8c7defecf3e1f47201a34f0f80847e9eb2
Pedro Furtado <pedro.nazare@pdcase.com.br>
2022-07-29 07:47:17 -0300

    adding layer map service

---
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package com.ppfurtado.sfgpetclinic.service.map;
+
+import com.ppfurtado.sfgpetclinic.model.Vet;
+import com.ppfurtado.sfgpetclinic.service.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+}
