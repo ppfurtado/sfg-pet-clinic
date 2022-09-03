4339cd639b635b9314c89315ffa7073cb3006a5d
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-09-02 23:55:11 -0300

    Added layout mapping '/find'

--- pet-clinic-web/src/main/java/com/ppfurtado/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/ppfurtado/sfgpetclinic/controller/OwnerController.java
@@ -16,7 +16,7 @@ public class OwnerController {
         this.ownerService = ownerService;
     }
 
-    @RequestMapping({"","/","/index", "/index.html"})
+    @RequestMapping({"","/","/index","/find", "/index.html"})
     public String listOwners(Model model){
 
         model.addAttribute("owners", ownerService.findAll());
