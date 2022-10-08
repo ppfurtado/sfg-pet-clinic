1c37b379aaee7e1ff64c9151a6c0f435b9093a32
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-10-07 23:32:45 -0300

    Added Visit Entity

--- pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/Pet.java
@@ -1,11 +1,12 @@
 package com.ppfurtado.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
     private String name;
@@ -18,6 +19,9 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "onwer_id")
     private Owner owner;
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -49,5 +53,13 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
