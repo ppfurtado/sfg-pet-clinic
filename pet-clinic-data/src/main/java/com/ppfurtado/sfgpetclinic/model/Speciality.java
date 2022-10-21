6399bee8983fbefbeef236087e6c69de2097296a
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-10-21 00:53:36 -0300

    added Project Lombok to project

--- pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/Speciality.java
@@ -1,19 +1,19 @@
 package com.ppfurtado.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "Specialities")
 public class Speciality extends BaseEntity {
 
     private String description;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
