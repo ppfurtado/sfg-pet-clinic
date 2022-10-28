7d2f10e1247822caf951cfd217b527b77ae8a282
Pedro Furtado <furtado.pedropaulo@gmail.com>
2022-10-28 00:29:44 -0300

    fixed annotation lombok

--- pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/ppfurtado/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.ppfurtado.sfgpetclinic.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
